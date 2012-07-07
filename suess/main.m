a667940463441b086ff2f78389373f084b77ff38
Cory Kilger <cmkilger@gmail.com>
2012-07-06 23:16:19 -0700

    Initial Commit

---
+++ suess/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  suess
+//
+//  Created by Cory Kilger on 7/6/12.
+//  Copyright (c) 2012 Cory Kilger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
