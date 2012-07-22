838a95fca813b9f347927ad897c4e7789d40a4e8
Cory Kilger <cmkilger@gmail.com>
2012-07-21 22:02:01 -0700

    Adding common header.

---
+++ suess/suess.h
@@ -0,0 +1,24 @@
+//
+//  suess.h
+//  suess
+//
+//  Created by Cory Kilger on 7/21/12.
+//  Copyright (c) 2012 Cory Kilger. All rights reserved.
+//
+
+#ifndef suess_Suess_h
+#define suess_Suess_h
+
+#include "SUType.h"
+#include "SUTokenizer.h"
+#include "SUList.h"
+#include "SUIterator.h"
+#include "SUString.h"
+#include "SUFunction.h"
+#include "SUStatement.h"
+#include "SUProgram.h"
+#include "SURange.h"
+#include "SUError.h"
+#include "SUVariable.h"
+
+#endif
